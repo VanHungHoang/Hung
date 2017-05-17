b5d951dfa3edd34725988d938e3aeacd37c8dcef
VanHungHoang <hoanghung.mta@gmail.com>
2017-05-18 01:43:32 +0700

    Projects
    Projects

---
+++ TodoNg2/my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { AdminModule } from './admin/admin.module';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
+import { BackendApiService } from './backend-api.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AdminModule
+  ],
+  providers: [BackendApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
