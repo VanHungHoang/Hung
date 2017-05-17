1d0c787133fc4af753400a048a54304da63febeb
VanHungHoang <hoanghung.mta@gmail.com>
2017-05-17 18:20:13 +0700

    WebApp
    WebApp

---
+++ WebApp/WebApp/Scripts/app/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'opengamelist',
+    template: `
+        <div>
+            <h1>OpenGameList</h1>
+            <div>Work in progress...</div>
+        </div>
+    `
+})
+export class AppComponent{
+    pageTitle: string = "Hello Angular2!";
+}
---
+++ WebApp/WebApp/Scripts/app/app.module.ts
@@ -0,0 +1,21 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import "rxjs/Rx";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    // modules
+    imports: [BrowserModule, FormsModule, HttpModule],
+
+    // directives, components, and pipes
+    declarations: [AppComponent],
+
+    // providers
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
